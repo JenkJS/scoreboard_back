ac002f1816aab7e221a7543169ac92686120dffc
JenkJS <jenkjobs@gmail.com>
2023-02-20 22:43:33 +0300

    fix: fixed

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const app = express()
 app.use(express.json())
 app.use(express.static('public'));
 app.use('/', router)
-// const DB_URL =
-//   'mongodb+srv://admin:admin@scoreboard.1vgh3xq.mongodb.net/scoreboard?retryWrites=true&w=majority'
+const DB_URL =
+  'mongodb+srv://admin:admin@scoreboard.1vgh3xq.mongodb.net/scoreboard?retryWrites=true&w=majority'
 
 async function startApp() {
   try {
-    // await mongoose.set('strictQuery', false)
-    // await mongoose.connect(DB_URL).then(() => {
-    //   console.log('bd - ok')
-    // })
+    await mongoose.set('strictQuery', false)
+    await mongoose.connect(DB_URL).then(() => {
+      console.log('bd - ok')
+    })
     app.listen(PORT, () => console.log('Server started on PORT: ' + PORT))
   } catch(e) {
     console.log('Err', + e)
   }
 }
-export default startApp()
+startApp()
+export default app
