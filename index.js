11b9818410223957d22f0953fd67dd45299cda60
JenkJS <jenkjobs@gmail.com>
2023-02-20 23:32:34 +0300

    fix: fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from './router.js'
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(express.json())
